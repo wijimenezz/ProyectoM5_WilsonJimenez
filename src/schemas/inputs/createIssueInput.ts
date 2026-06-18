1d784682d0044bc0579af8be5fde48eaad6b8585
Wilson Jimenez <wijimenezz@gmailcom>
2026-06-18 11:31:06 -0500

    fix: Modificar nombre de schema para diferenciarlo del issue creado

---
+++ src/schemas/inputs/createIssueInput.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+export const createIssueSchema = z.object({
+  owner: z
+    .string()
+    .min(1)
+    .describe("dueño del Repositorio (empresa u organización)"),
+  repo: z.string().min(1).describe("Nombre del Repositorio"),
+  title: z
+    .string()
+    .min(3, { message: "El título debe tener al menos 3 caracteres." })
+    .describe("Título del issue (mínimo 3 caracteres)"),
+
+  body: z
+    .string()
+    .optional()
+    .describe("Cuerpo del issue en Markdown (opcional)"),
+});
+
+export type CreatedIssueInput = z.infer<typeof createIssueSchema>;
